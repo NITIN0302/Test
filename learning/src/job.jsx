0ebde3da7a3d18dd9b5560a20ed7943395aac45d
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-07-03 00:16:51 +0530

    Props

---
+++ learning/src/job.jsx
@@ -0,0 +1,12 @@
+function Job(props)
+{
+    return (
+        <>
+        <h1>{props.salary}</h1>
+        <h1>{props.position}</h1>
+        <h1>{props.company}</h1>
+        </>
+    );
+}
+
+export default Job;
