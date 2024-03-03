473cc3af0932d4c9185170bbcb03ab5f8a39c58e
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-03-03 23:42:14 +0530

    Try to create theme Toggler using tailwind

---
+++ Theme-Toggler/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useContext, useEffect } from "react";
+import { ThemeProvider } from "./context/theme.js";
+import Themebtn from "./component/themebtn.jsx";
+import Card from "./component/Card.jsx";
+import "./App.css";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  function darkTheme() {
+    setThemeMode("light");
+  }
+  function lightTheme() {
+    setThemeMode("dark");
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <Themebtn />
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
