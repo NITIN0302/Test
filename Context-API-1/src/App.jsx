3eda45f3533298caeac719f59b46c0c4acb36a8e
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-03-04 22:16:07 +0530

    Learning useContext method

---
+++ Context-API-1/src/App.jsx
@@ -0,0 +1,29 @@
+import Heading from "./Heading.jsx";
+import Section from "./Section.jsx";
+import Footer from "./Footer.jsx";
+
+export default function Page() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+        <Section level={3}>
+          <Footer>Footer Data</Footer>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
